9e3293c68fad5b34c74087de9c2413dffc8a4657
Will Burton <willburton@linney.com>
2023-01-13 15:17:52 +0000

    export has broken :(

--- vault.js
+++ vault.js
@@ -1,7 +1,7 @@
 const options = {
     apiVersion: 'v1', // default
     endpoint: 'http://localhost:8200', // default
-    token: '' // optional client token; can be fetched after valid initialization of the server
+    token: 'hvs.KCwDJJNoXYYVajnMIzc16mAp' // optional client token; can be fetched after valid initialization of the server
 }
 
 const vault = require("node-vault")(options);
